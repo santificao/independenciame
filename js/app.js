5787642bdf947456bebdfdddf4ea69a177142152
Santi <santiagosernaalmaida@gmail.com>
2019-02-05 19:35:19 +0100

    Primer commit, pagina de presentación finalizada

---
+++ js/app.js
@@ -0,0 +1,14 @@
+//JQuery
+$(document).ready(function() {
+
+    /**Efectos personalizados para plugin lightBox */
+    lightbox.option({
+        'albumLabel': "Imagen %1 de %2"
+    });
+
+    /**Efecto para mostrar/ocultar menú móvil*/
+    $(".menu-movil").click(function() {
+        $(".menu").slideToggle(300);
+    });
+
+});
